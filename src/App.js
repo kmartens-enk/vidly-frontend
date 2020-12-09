8c79c173a3c94dd57d846c269e82288cef2635a3
k.martens <k.martens@enk.nl>
2020-12-09 20:35:33 +0100

    form excersies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import './App.css';
-import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
-import Movies from './components/movies';
-import MovieForm from './components/movieForm';
-import Customers from './components/customers';
-import Rentals from './components/rentals'
-import NotFound from './components/notFound';
-import NavBar from './components/navbar';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navbar";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
-    <NavBar/>
-    <main className="container m-2">
-      <Switch>
-      <Route path="/customers" component={Customers}/>
-      <Route path='/rentals' component={Rentals}/>
-      <Route path='/movies/:id' component={MovieForm}/>
-      <Route path="/movies" component={Movies}/>
-      <Redirect from="/" exact to="/movies"/>
-      <Redirect to="" component={NotFound}/>
-      </Switch>
-    </main>
+      <NavBar />
+      <main className="container m-2">
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="" component={NotFound} />
+        </Switch>
+      </main>
     </React.Fragment>
   );
 }
